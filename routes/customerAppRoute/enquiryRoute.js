2221f73aee8d4c06f88af7716efe39c421771c4d
KIRAN PRAKASH UGALE <141510294+kiranugale2o@users.noreply.github.com>
2025-12-03 22:36:52 +0530

    Add lead notification route to enquiryRoute

--- routes/customerAppRoute/enquiryRoute.js
+++ routes/customerAppRoute/enquiryRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { add, getAll, getBookingOnly, getVisitsOnly } from "../../controllers/customerAppController/enquiryController.js";
+import { add, addLeadNotification, getAll, getBookingOnly, getVisitsOnly } from "../../controllers/customerAppController/enquiryController.js";
 import { getPaymentList } from "../../controllers/sales/customerController.js";
 
 const router = express.Router();
@@ -8,4 +8,6 @@ router.get('/get',getAll)
 router.get('/getVisitProperty',getVisitsOnly)
 router.get('/getBookingProperty',getBookingOnly)
 router.get("/payment/get/:id", getPaymentList);
+router.post("/add/notify",addLeadNotification
+)
 export default router;
