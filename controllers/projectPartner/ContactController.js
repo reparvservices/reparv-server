7a2e186c3759882f8f8385775c13410d7dfed2af
KIRAN PRAKASH UGALE <141510294+kiranugale2o@users.noreply.github.com>
2025-12-18 20:46:13 +0530

    Remove email from contact form submission

--- controllers/projectPartner/ContactController.js
+++ controllers/projectPartner/ContactController.js
@@ -4,18 +4,18 @@
 import db from "../../config/dbconnect.js";
 
 export const submitContactForm = (req, res) => {
-  const { fullName, email, contact, message } = req.body;
+  const { fullName, contact, message } = req.body;
 
   // Basic validation
-  if (!fullName || !email || !contact || !message) {
+  if (!fullName ||  !contact || !message) {
     return res.status(400).json({ message: "All fields are required" });
   }
 
   const sql =
-    "INSERT INTO contact_messages (full_name, email, contact, message) VALUES (?, ?, ?, ?)";
+    "INSERT INTO messages (fullname,contact, message) VALUES (?, ?, ?)";
 
   // Using mysql2 callback style
-  db.query(sql, [fullName, email, contact, message], (err, results) => {
+  db.query(sql, [fullName,contact, message], (err, results) => {
     if (err) {
       console.error("Contact Form Error:", err);
       return res.status(500).json({ message: "Server error" });
@@ -27,4 +27,47 @@ export const submitContactForm = (req, res) => {
     });
   });
 };
+
+
+
+
+export const createSchedule = (req, res) => {
+  const { fullName, contact, date, time, message } = req.body;
+console.log(req.body);
+
+  // Basic validation
+  if (!fullName || !contact || !date || !time) {
+    return res.status(400).json({
+      success: false,
+      message: "All required fields missing",
+    });
+  }
+
+  const sql = `
+    INSERT INTO schedule_requests 
+    (full_name, contact, scheduled_date, scheduled_time, message)
+    VALUES (?, ?, ?, ?, ?)
+  `;
+
+  // mysql2 callback style
+  db.query(
+    sql,
+    [fullName, contact, date, time, message || null],
+    (err, results) => {
+      if (err) {
+        console.error("Create Schedule Error:", err);
+        return res.status(500).json({
+          success: false,
+          message: "Server Error",
+        });
+      }
+
+      res.status(201).json({
+        success: true,
+        message: "Schedule request submitted successfully",
+      });
+    }
+  );
+};
+
 
