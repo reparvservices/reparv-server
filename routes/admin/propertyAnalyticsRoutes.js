4a62b10cc2f6587847ff5dd664d0932abca4a105
kiran ugale <kiranugale129@gmail.com>
2025-12-23 15:49:13 +0530

    add property Analytics Routes

---
+++ routes/admin/propertyAnalyticsRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { addVisitor, getTotalVisitors } from "../../controllers/admin/propertyAnalyticsController.js";
+
+const router = express.Router();
+
+router.post("/addvisits",addVisitor)
+router.get("/getvisits",getTotalVisitors)
+
+export default router;
