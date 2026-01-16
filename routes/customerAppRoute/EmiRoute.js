b1016587344d12cb1857c5fa21a6cfac9c449a61
kiranugale2o <kiranugale129@gmail.com>
2026-01-17 02:23:09 +0530

    implement google login

--- routes/customerAppRoute/EmiRoute.js
+++ routes/customerAppRoute/EmiRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { submitEmiForm } from "../../controllers/customerAppController/loanEmiController.js";
+import { getLoansByUserId, getUserLoanCounts, submitEmiForm } from "../../controllers/customerAppController/loanEmiController.js";
 
 
 const router = express.Router();
 
 router.post('/emiform',submitEmiForm)
+router.get("/counts/:user_id", getUserLoanCounts);
+router.get("/loan-applications/:user_id", getLoansByUserId);
 
 export default router;
