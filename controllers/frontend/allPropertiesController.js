3e42ba0d5d2342a23f9ce1113a2dd7daf412a600
kandalkarrohan2020 <kandalkarrohan2020@gmail.com>
2026-01-27 14:48:12 +0530

    Create Top-Picks and hot-deal Routes and Controllers

--- controllers/frontend/allPropertiesController.js
+++ controllers/frontend/allPropertiesController.js
@@ -63,3 +63,69 @@ export const getAllByCity = (req, res) => {
     res.json(formatted);
   });
 };
+
+// ** Fetch All City **
+export const getHotDealProperties = (req, res) => {
+  const city = req.params.city;
+  if (!city) {
+    return res.status(401).json({ message: "City Not Selected!" });
+  }
+  const sql = `SELECT * FROM properties WHERE status='Active' AND approve='Approved' AND hotDeal = 'active' AND city = ? ORDER BY RAND() `;
+  db.query(sql, [city], (err, result) => {
+    if (err) {
+      console.error("Error fetching:", err);
+      return res.status(500).json({ message: "Database error", error: err });
+    }
+
+    // safely parse JSON fields
+    const formatted = result.map((row) => {
+      let parsedType = null;
+      try {
+        parsedType = row.propertyType ? JSON.parse(row.propertyType) : [];
+      } catch (e) {
+        console.warn("Invalid JSON in propertyType:", row.propertyType);
+        parsedType = [];
+      }
+
+      return {
+        ...row,
+        propertyType: parsedType,
+      };
+    });
+
+    res.json(formatted);
+  });
+};
+
+// ** Fetch All City **
+export const getTopPicksProperties = (req, res) => {
+  const city = req.params.city;
+  if (!city) {
+    return res.status(401).json({ message: "City Not Selected!" });
+  }
+  const sql = `SELECT * FROM properties WHERE status='Active' AND approve='Approved' AND topPicksStatus = 'active' AND city = ? ORDER BY RAND() `;
+  db.query(sql, [city], (err, result) => {
+    if (err) {
+      console.error("Error fetching:", err);
+      return res.status(500).json({ message: "Database error", error: err });
+    }
+
+    // safely parse JSON fields
+    const formatted = result.map((row) => {
+      let parsedType = null;
+      try {
+        parsedType = row.propertyType ? JSON.parse(row.propertyType) : [];
+      } catch (e) {
+        console.warn("Invalid JSON in propertyType:", row.propertyType);
+        parsedType = [];
+      }
+
+      return {
+        ...row,
+        propertyType: parsedType,
+      };
+    });
+
+    res.json(formatted);
+  });
+};
--- routes/frontend/allPropertiesRoutes.js
+++ routes/frontend/allPropertiesRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {getAll, getAllByCity} from "../../controllers/frontend/allPropertiesController.js";
+import {getAll, getAllByCity, getHotDealProperties, getTopPicksProperties} from "../../controllers/frontend/allPropertiesController.js";
 
 const router = express.Router();
 
 router.get("/", getAll);
 router.get("/:city", getAllByCity);
+router.get("/hot-deal/:city", getHotDealProperties);
+router.get("/top-picks/:city", getTopPicksProperties);
 
 export default  router;
