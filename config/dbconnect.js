daeef434b0ff9fbff29ffa576de3bdee468fb59d
vishalpatilmaske <vishalpatilmaske@gmail.com>
2026-02-26 21:11:43 +0530

    update te db promiss

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,15 +1,13 @@
 import mysql from "mysql2";
 
-const db = mysql
-  .createPool({
-    connectionLimit: 20,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    connectTimeout: 10000,
-  })
-  .promise();
+const db = mysql.createPool({
+  connectionLimit: 20,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  connectTimeout: 10000,
+});
 
 export default db;
