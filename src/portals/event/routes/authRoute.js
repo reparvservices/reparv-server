c287af66cb8fedbd759fb10be43bdf3c09a89bb4
kiranugale2o <kiranugale129@gmail.com>
2026-04-28 11:44:08 +0530

    add delete url

--- src/portals/event/routes/authRoute.js
+++ src/portals/event/routes/authRoute.js
@@ -1,8 +1,6 @@
 import express from "express";
-import {
-  sendUserOtp,
-  verifyUserOtp,
-} from "../controllers/authContoller.js";
+import { sendUserOtp, verifyUserOtp } from "../controllers/authContoller.js";
+import db from "#db";
 const router = express.Router();
 
 //Send OTP
@@ -10,5 +8,33 @@ router.post("/send-otp", sendUserOtp);
 
 // Verify OTP
 router.post("/verify-otp", verifyUserOtp);
+// routes/user.js
+
+router.delete("/delete-account", (req, res) => {
+  const userId = req.user.id;
+
+  const deleteUserQuery = "DELETE FROM eventUsers WHERE id = ?";
+
+  db.query(deleteUserQuery, [userId], (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: "Error deleting user",
+      });
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Account deleted successfully",
+    });
+  });
+});
 
 export default router;
